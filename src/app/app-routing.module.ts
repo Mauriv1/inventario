aa56b6a8ee0c65a1d7f1ebff046875474037b098
Mauriv1 <Mauriv1@users.noreply.github.com>
2022-10-04 19:25:40 -0500

    Primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /* {path:'', pathMatch:'full', redirectTo:'add'},
+  {path:'add',component:AddComponent},
+  {path:'list',component:ListComponent },
+  {path:'edit/:id_product', component:EditComponent } */
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
